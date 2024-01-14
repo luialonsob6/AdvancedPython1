cfb7dfc4a91b49fd713860b2bd5dea261892a8fc
luialonsob6 <luis.alonso6@alumni.esade.edu>
2024-01-14 17:09:01 +0100

    Importing_click

--- Base_line_script.py
+++ Base_line_script.py
@@ -2,12 +2,17 @@
 Script to make updates in github
 """
 import pandas as pd
+import click
+
+@click.command(short_help="parser to import dataset")
+@click.option("-i","--input",required=True, help="File to import")
 
 def main():
     """
     Main Function
     """
-    pass
+    df = pd.read_csv(input)
+    print(df.shape)
 
 if __name__=="__main__":
     main()
