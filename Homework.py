6c6bdf30b479ce3cd6abce11ff3b9ddd5987b0d0
luialonsob6 <luis.alonso6@alumni.esade.edu>
2024-01-14 18:43:00 +0100

    Filtering_option_pdb

--- Homework.py
+++ Homework.py
@@ -29,27 +29,33 @@ class FilteringClass:
 @click.command(short_help="parser to import dataset")
 @click.option("-i","--input",required=True, help="File to import")
 @click.option("-o", "--output", default="outputs", help="Path to the output folder")
+@click.optiom("-f", "--filtering", is_Flag=True, help = "Set a filtering or not")
 @click.option("-g", "--genre", help="Filter by a desired genre", required=True)
 @click.option("-y", "--year", help="Filter by a desired minimum year", required=True)
 
 
-def main(input, output, genre, year):
+def main(input, output, filtering, genre, year):
     """
     Main Function
     """   
     try:
         df = pd.read_csv(input)
         print(f"The file imported is:'{input}'")
-    except FileNotFoundError:
-        print(f"The file:'{input}' was not found")
+    except FileNotFoundError as e:
+        print(f"The file:'{e}' was not found")
     
+    print(df.shape)
+    print(df.info())
     
-    Filter_genre = FilteringClass(df).by_genre(genre)
-    Filter_exact_year = FilteringClass(df).exact_year(year)
+    import pdb;pdb.set_trace()
+
+    if filtering:
+        print("I am going to print the shape from the Dataset that correspond to the genre:", genre, "and the year:", year)
+        Filter_genre = FilteringClass(df).by_genre(genre) 
+        Filter_exact_year = FilteringClass(df).exact_year(year)
 
     filtered_df = pd.concat([Filter_genre, Filter_exact_year], axis =0).drop_duplicates()
 
-    print(df.shape())
     return filtered_df.shape()
 
 if __name__=="__main__":
