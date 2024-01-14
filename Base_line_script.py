2e566eed4f46df494f3c6c91a34dca0b4bd2d636
luialonsob6 <luis.alonso6@alumni.esade.edu>
2024-01-14 17:10:31 +0100

    New_class_for_filtering

--- Base_line_script.py
+++ Base_line_script.py
@@ -4,6 +4,20 @@ Script to make updates in github
 import pandas as pd
 import click
 
+class FilteringClass:
+    """
+    Class for filtering
+    """
+
+    def __init__(self, df):
+        self.df = df
+    
+    def filter_price(self,price):
+        """
+        filter by price
+        """
+        return self.df[self.df["Price Starting With ($)"] < price]
+
 @click.command(short_help="parser to import dataset")
 @click.option("-i","--input",required=True, help="File to import")
 
@@ -12,7 +26,8 @@ def main():
     Main Function
     """
     df = pd.read_csv(input)
-    print(df.shape)
+    result = FilteringClass(df).filter_price(12)
+    print(result.shape)
 
 if __name__=="__main__":
     main()
