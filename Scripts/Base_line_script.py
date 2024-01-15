b3e4b8bfcff6b3c8ad093c5c421a2b0e347eccbf
luialonsob6 <luis.alonso6@alumni.esade.edu>
2024-01-15 17:04:44 +0100

    Datset_test

--- Scripts/Base_line_script.py
+++ Scripts/Base_line_script.py
@@ -17,6 +17,16 @@ class FilteringClass:
         filter by price
         """
         return self.df[self.df["Price Starting With ($)"] < price]
+    
+def load_dataset(filename):
+    """
+    Function to load dataset
+    """
+    extension = filename.rsplit(".",1)[-1]
+    if extension == "csv":
+        return pd.read_csv(filename)
+    else:
+        raise TypeError(f"The extension is {extension} and not csv, try again") #Hacemos un raise del error(lo mismo que en el try and except)
 
 @click.command(short_help="parser to import dataset")
 @click.option("-i","--input",required=True, help="File to import")
@@ -25,7 +35,7 @@ def main(input):
     """
     Main Function
     """     
-    df = pd.read_csv(input)
+    df = load_dataset(input)
     result = FilteringClass(df).filter_price(12)
     print(result.shape)
 
---
+++ tests/test_dataset.py
@@ -0,0 +1,26 @@
+"""
+Test loading of the dataset
+"""
+
+import unittest
+from Scripts.Base_line_script import load_dataset
+
+class TestDataset(unittest.TestCase):
+    """
+    Class to test the dataset input in different ways
+    """
+    def setUp(self):    #The setUp function allows us to ... preguntar Chat GPT
+        """
+        Path to dataset
+        """
+        self.path = "Datasets/BooksDatasetClean.csdsfsadv"
+    
+    def test_extension_fail(self):
+        """
+        Test for the extension of the dataset
+        """
+        with self.assertRaises(TypeError):
+            load_dataset(self.path)
+
+
+
